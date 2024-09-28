f2ec1eab940325081d1cfeca42f729a21df66743
asd417 <loctitematt417@gmail.com>
2024-09-28 17:26:39 -0400

    fixed sword

--- Assets/Generated/RotateAroundPlayer.cs
+++ Assets/Generated/RotateAroundPlayer.cs
@@ -1,23 +1,29 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RotateAroundPlayer : MonoBehaviour
 {
     [Tooltip("Speed at which the sprite rotates around the player.")]
-    [SerializeField] private float rotationSpeed = 5f;
+    private float rotationSpeed = 1f;
 
     private Transform targetPlayer;
     private bool isAttached = false;
+    private float angle_euler = 0;
+    private int dist = 2;
 
     void Update()
     {
         if (isAttached && targetPlayer != null)
         {
-            transform.RotateAround(targetPlayer.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+            angle_euler = (angle_euler + rotationSpeed) % 360;
+            transform.eulerAngles = new Vector3 (0,0, angle_euler);
+            transform.position = targetPlayer.position + new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle_euler) * dist, Mathf.Sin(Mathf.Deg2Rad * angle_euler) * dist, 0);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Collided");
         if (collision.CompareTag("Player"))
         {
             targetPlayer = collision.transform;
