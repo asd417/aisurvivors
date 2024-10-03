78739a1bc5370542d1d412bfa38b0390ee5e47e7
tfriend123 <tfriend@conncoll.edu>
2024-10-03 11:41:23 -0400

    Enemy Spawn update
    Still needs work

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    // Prefab of the enemy sprite to instantiate
+    public GameObject spriteEnemyPrefab;
+    public int spawnx1;
+    public int spawny1;
+    public int spawnx2;
+    public int spawny2;
+    public int numberEnemy;
+
+    void Start()
+    {
+        // Instantiate a finite number of sprites and add them to the list
+        for (int i = numberEnemy; i <= numberEnemy; i++){
+            Vector3 initialPosition;
+            int spawnPoint = Random.Range(0,2);
+
+            if (spriteEnemyPrefab != null)
+            {
+                if (spawnPoint == 0){
+                    initialPosition = new Vector3(spawnx1, spawny1);
+                }
+                else if (spawnPoint == 1){
+                    initialPosition = new Vector3(spawnx2, spawny2);
+                }
+                else{
+                    initialPosition = new Vector3(0, 0);
+                }
+                
+            
+            // Instantiate the sprite prefab at a valid position on the 2D NavMesh
+                GameObject newEnemySprite = Instantiate(spriteEnemyPrefab, initialPosition, Quaternion.identity);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
