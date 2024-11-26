eb57fe6a33bb8ebd6966261d67d2cb6ccd295863
tfriend123 <tfriend@conncoll.edu>
2024-11-26 11:23:35 -0500

    More Updates

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -6,19 +6,28 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     private float delayBeforeChange = 5f;
+    public bool isControls = false;
 
     private void Start()
     {
-        SoundManager.instance.StopAllSounds();
-        StartCoroutine(PlayIntroAnimationSounds());
-        StartCoroutine(ChangeSceneToLobbyAfterDelay());   
+        if (!isControls){
+            SoundManager.instance.StopAllSounds();
+            StartCoroutine(PlayIntroAnimationSounds());
+            StartCoroutine(ChangeSceneToLobbyAfterDelay());  
+        } 
     }
 
     private IEnumerator ChangeSceneToLobbyAfterDelay()
     {
         yield return new WaitForSeconds(delayBeforeChange);
-        SceneManager.LoadScene("Level2 1");
-        SoundManager.instance.FadeOut("MenuAnimation3", 3f);
+        if (isControls){
+            Destroy(gameObject);
+        }
+        else{
+            SceneManager.LoadScene("Level2 1");
+            SoundManager.instance.FadeOut("MenuAnimation3", 3f);
+        }
+        
 
     }
 
@@ -27,14 +36,17 @@ public class SceneChanger : MonoBehaviour
 
     private IEnumerator PlayIntroAnimationSounds() // used for Title scene change (intro animation)
     {
-        // trigger 3rd background sfx first
-        SoundManager.instance.Play("MenuPlayAnimation3");
+        if (!isControls){
+            // trigger 3rd background sfx first
+            SoundManager.instance.Play("MenuPlayAnimation3");
 
-        // slashes sfx
-        SoundManager.instance.Play("MenuPlayAnimation1");
-        yield return new WaitForSeconds(2f); // duration to wait between sounds
+            // slashes sfx
+            SoundManager.instance.Play("MenuPlayAnimation1");
+            yield return new WaitForSeconds(2f); // duration to wait between sounds
 
-        // crescendo
-        SoundManager.instance.Play("MenuPlayAnimation2"); // Replace with your second sound name
+            // crescendo
+            SoundManager.instance.Play("MenuPlayAnimation2"); // Replace with your second sound name
+        }
+        
     }
 }
